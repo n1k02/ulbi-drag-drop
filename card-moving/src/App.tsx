0840038a457c5f574fadcd3f7a7c9074f1aad4e2
n1k02 <ghostwarriorn@gmail.com>
2023-02-22 18:18:48 +0500

    finish

--- card-moving/src/App.tsx
+++ card-moving/src/App.tsx
@@ -12,26 +12,54 @@ function App() {
     {id:2, order:2, text: 'CARD 2'},
     {id:3, order:3, text: 'CARD 3'},
   ]);
+  const [currentCard, setCurrentCard] = useState<ICard>();
 
   const dragStartHandler = (e: React.DragEvent<HTMLDivElement>, card: ICard) => {
-
+    console.log('dragStart', card)
+    setCurrentCard(card)
   }
   const dragLeaveHandler = (e: React.DragEvent<HTMLDivElement>) => {
-
+    console.log('dragLeave')
+    const el = e.target as HTMLElement
+    el.style.background = 'none'
   }
   const dragEndHandler = (e: React.DragEvent<HTMLDivElement>) => {
 
   }
   const dragOverHandler = (e: React.DragEvent<HTMLDivElement>) => {
-
+    e.preventDefault()
+    console.log('dragOver')
+    const el = e.target as HTMLElement
+    el.style.background = 'lightgray'
   }
   const dragDropHandler = (e: React.DragEvent<HTMLDivElement>, card: ICard) => {
+    e.preventDefault()
+    console.log('drop', card)
+    setCardList(cardList.map(c => {
+      if(c.id === card.id) {
+        return {...c, order: currentCard?.order || c.order}
+      }
+      if(c.id === currentCard?.id) {
+        return {...c, order: card.order}
+      }
+      return c
+    }))
+    const el = e.target as HTMLElement
+    el.style.background = 'none'
 
+  }
+
+  const sortCards = (a: ICard, b: ICard) => {
+    if(a.order > b.order) {
+      return 1
+    } else {
+      return -1
+    }
   }
 
   return (
     <div className='App'>
-      {cardList.map((card) => (
+      {cardList.sort(sortCards).map((card) => (
           <div
               key={card.id}
               className={'card'}
